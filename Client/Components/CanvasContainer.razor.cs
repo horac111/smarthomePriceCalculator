89bbbdb70298cae260c7addd491eb35795209200
Jiri Horacek <horac111@seznam.cz>
2023-05-08 11:00:50 +0200

    Better import to react  - still error with fluent ui icons

---
+++ Client/Components/CanvasContainer.razor.cs
@@ -0,0 +1,41 @@
+﻿using Blazored.Modal.Services;
+using CanvasComponent.Convertors;
+using CanvasComponent.Facade;
+using CanvasComponent.Model;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace SmartHomePriceCalculatorClient.Components
+{
+    public partial class CanvasContainer : ComponentBase
+    {
+        [Parameter]
+        public string Project { get; set; }
+
+        [Inject]
+        public IJSRuntime JS { get; set; }
+
+        [Inject]
+        public IModalService ModalService { get; set; }
+
+        private CanvasFacade canvasFacade
+        {
+            get
+            {
+                JsonSerializerOptions options = new()
+                {
+                    IgnoreReadOnlyProperties = true,
+                    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+                    IncludeFields = true
+                };
+                options.Converters.Add(new ProjectConverter());
+                options.Converters.Add(new ISmartDeviceConverter());
+                options.Converters.Add(new LineConverter());
+                options.Converters.Add(new RoomConverter());
+                return new(JsonSerializer.Deserialize<Project>(Project, options), JS, ModalService);
+            }
+        }
+    }
+}
--- smarthome-price-calculator-react/src/App.js
+++ smarthome-price-calculator-react/src/App.js
@@ -2,39 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import './CanvasContainer'
 import { CanvasContainer } from './CanvasContainer';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import project from './project.json'
 
 function App() {
-  console.log(JSON.stringify(project));
-  const [nextCounterIndex, setNextCounterIndex] = useState(1);
-  const [blazorCounters, setBlazorCounters] = useState([]);
-  const addBlazorCounter = () => {
-    const index = nextCounterIndex;
-    setNextCounterIndex(index + 1);
-    setBlazorCounters(blazorCounters.concat([{
-      title: `ahoj ${index}`,
-      project: project
-    }]));
-  };
-  const removeBlazorCounter = () => {
-    setBlazorCounters(blazorCounters.slice(0, -1));
-  };
+  const [shouldRenderCanvas, setShouldRenderCanvas] = useState(false);
+  useEffect(() => { setTimeout(() => setShouldRenderCanvas(true), 5000);});
   return (
     <div className="App">
       <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <button onClick={addBlazorCounter}>Add Blazor counter</button> &nbsp;
-          <button onClick={removeBlazorCounter}>Remove Blazor counter</button> &nbsp;
-        </p>
-
-        {blazorCounters.map(counter =>
-          <div key={counter.title}>
-            <CanvasContainer project={ JSON.stringify(counter.project)}>
-            </CanvasContainer>
-          </div>
-        )}
+           { shouldRenderCanvas ? <CanvasContainer project={ JSON.stringify(project)} /> : null}
       </header>
     </div>
   );
